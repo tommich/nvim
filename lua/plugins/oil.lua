0da27a686971a69a01305d2ace6efcbfd61fa8d7
tommich <tommich91@gmail.com>
2024-06-16 21:50:39 +0100

    remove insert mode escape

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    require('oil').setup({
+      default_file_explorer = false,
+      view_options = { show_hidden = true },
+    })
+  end
+}
---
+++ lua/plugins/pretty-fold.lua
@@ -0,0 +1,5 @@
+return { 'anuvyklack/pretty-fold.nvim',
+   config = function(_, _)
+      require('pretty-fold').setup()
+   end
+}
